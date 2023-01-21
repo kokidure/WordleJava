761a12c3f9cd7b55d07ef578d42e959ab8e32768
Jorge Dure <jorge.dure@redfin.com>
2023-01-21 16:23:33 -0300

    Wordle en Java

---
+++ src/main/java/com/wordle/app/Main.java
@@ -0,0 +1,63 @@
+package com.wordle.app;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        int retries = 5;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("WORDLE: adivina la palabra de 5 letras.");
+        System.out.println("Tenes 5 intentos.");
+
+        while (retries > 0) {
+
+            String input = sc.nextLine();
+            String target = "avion";
+
+            System.out.println(checkString(input, target));    
+
+            if (input.equalsIgnoreCase(target)) {
+                System.out.println("FELICIDADES! Acertaste la palabra.");
+                break;
+            }
+
+            retries --;
+            System.out.println("Te quedan " + retries + " intentos.");
+        }
+
+        sc.close();
+    }
+
+    public static String checkString(String input, String target) {
+        StringBuilder sb = new StringBuilder();
+
+        if (input.length()!=5){
+            System.out.println("Debes ingresar una palabra de 5 letras.");
+            return sb.toString();
+        }
+
+        for (char c : input.toCharArray()){
+            String currentChar = String.valueOf(c);
+            if (target.contains(currentChar)) {
+                if (input.indexOf(currentChar) == target.indexOf(currentChar)) {
+                    sb.append("{" + c + "}");
+                }
+                else {
+                    sb.append("(" + c + ")");
+                }
+            }
+            else {
+                sb.append(c);
+            }
+
+        }
+        return sb.toString();
+    }
+}
